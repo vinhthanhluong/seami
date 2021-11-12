60dad7df76a1c4a56571e6c9dca19ac7a9898c5e
thành <vinhthanhluong@gmail.com>
2021-11-12 23:37:09 +0700

    update 12-11 lan 3

--- template/js/modules/LightGalleryModule.js
+++ template/js/modules/LightGalleryModule.js
@@ -2,8 +2,8 @@ export default function LightGalleryModule(){
     if ($('#lightgallery').length) {
 
         $("#lightgallery").lightGallery({
-            selector: ".zoom-lightgallery",
-            // thumbnail: true,
+            // selector: ".zoom-lightgallery",
+            thumbnail: true,
             // animateThumb: false,
             // showThumbByDefault: false
             download: false,
